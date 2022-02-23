7439e407560176e7da66fd348505528d84a4bdf1
Inayá Ferreira <inayavitoria10@gmail.com>
2022-02-23 13:17:00 -0300

    Update AddTask.jsx

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -6,7 +6,9 @@ const AddTask = () => {
     return (
         <div className="add-task-container">
             <input className="add-task-input" type="text" />
-            <Button>Adicionar</Button>
+            <div className="add-task-button-container">
+                <Button>Adicionar</Button>
+            </div>
         </div>
     );
 };
